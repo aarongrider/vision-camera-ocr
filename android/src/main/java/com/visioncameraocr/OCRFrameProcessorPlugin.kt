402f6dfef3b9653e1c13e61761d118d9ad0b3d73
carel155 <carelmartten@gmail.com>
2023-05-02 22:45:57 +0300

    Fix android empty elements array

--- android/src/main/java/com/visioncameraocr/OCRFrameProcessorPlugin.kt
+++ android/src/main/java/com/visioncameraocr/OCRFrameProcessorPlugin.kt
@@ -60,6 +60,8 @@ class OCRFrameProcessorPlugin: FrameProcessorPlugin("scanOCR") {
             elementMap.putString("text", element.text)
             elementMap.putArray("cornerPoints", element.cornerPoints?.let { getCornerPoints(it) })
             elementMap.putMap("frame", getFrame(element.boundingBox))
+
+            elementArray.pushMap(elementMap)
         }
         return elementArray
     }
